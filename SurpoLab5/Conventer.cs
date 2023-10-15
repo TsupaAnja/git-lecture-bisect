cff9ac48384b708905d5732a2eac1f7a8c1cf5c9
TsupaAnja <tsupaanja2015@gmail.com>
2023-10-16 00:29:45 +0300

    Добавьте файлы проекта.

---
+++ SurpoLab5/Conventer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurpoLab5
+{
+    internal class CurrencyConventer
+    {
+        public decimal Convert(decimal amount, decimal convertionRate)
+        {
+            return amount * convertionRate;
+        }
+    }
+}
---
+++ SurpoLab5/Program.cs
@@ -0,0 +1,12 @@
+﻿using SurpoLab5;
+
+class Program
+{
+
+    static void Main()
+    {
+        CurrencyConventer conventer = new CurrencyConventer();
+        decimal convertedAmount = conventer.Convert(100, 0.85M);
+        Console.WriteLine(convertedAmount.ToString());
+    }
+}
