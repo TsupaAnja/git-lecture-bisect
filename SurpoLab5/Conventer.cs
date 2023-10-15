db4039e86a79746ceb270277c575cadf675dee79
TsupaAnja <tsupaanja2015@gmail.com>
2023-10-16 00:59:44 +0300

    Historical rates lookup

--- SurpoLab5/Conventer.cs
+++ SurpoLab5/Conventer.cs
@@ -8,6 +8,14 @@ namespace SurpoLab5
 {
     internal class CurrencyConventer
     {
+        private Dictionary<DateTime, decimal> historicalRates = new Dictionary<DateTime, decimal>
+        {
+            {new DateTime(2022, 1,1), 0.85M},
+            {new DateTime(2022, 2,1), 0.86M},
+            {new DateTime(2022, 3,1), 0.87M}
+        };
+
+        
         public decimal Convert(decimal amount, decimal convertionRate)
         {
             ValidateRate(convertionRate);
@@ -17,6 +25,13 @@ namespace SurpoLab5
         private void ValidateRate(decimal rate)
         {
             // Здесь нет реальной проверки, прото заглушка для возможных будущих проверок
+        }
+
+        public decimal GetHistoricalRate(DateTime date)
+        {
+            return historicalRates.TryGetValue(date, out decimal rate) ? 
+                rate : 
+                throw new ArgumentException("Заданное историческое значение не было найдено в БД");
         }
     }
 }
--- SurpoLab5/Program.cs
+++ SurpoLab5/Program.cs
@@ -6,7 +6,13 @@ class Program
     static void Main()
     {
         CurrencyConventer conventer = new CurrencyConventer();
-        decimal convertedAmount = conventer.Convert(100, 0.85M);
-        Console.WriteLine(convertedAmount.ToString());
+        // decimal convertedAmount = conventer.Convert(100, 0.85M);
+        // Console.WriteLine(convertedAmount.ToString());
+
+        DateTime historicalDate = new DateTime(2022, 2, 1);
+        decimal historicalRate = conventer.GetHistoricalRate(historicalDate);
+        decimal conventerAmount = conventer.Convert(100, historicalRate);
+        Console.WriteLine(conventerAmount.ToString());
+
     }
 }
